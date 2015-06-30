15fcc70cc1bdb0662728b4fcfdd73616b3a6e7c3
James Hagerman <james.hagerman@gmail.com>
2015-06-30 02:16:39 -0700

    Making it so that the update.sh script will not update if it gets run accidently.

--- public/update.sh
+++ public/update.sh
@@ -2,7 +2,9 @@
 # Fetch the latest versions of three.js, VRControls and VREffect from the
 # three.js repository.
 
-curl -o js/lib/VRControls.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/VRControls.js
-curl -o js/lib/VREffect.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/effects/VREffect.js
-curl -o js/lib/three.min.js http://threejs.org/build/three.min.js
-curl -o js/lib/webvr-polyfill.js https://raw.githubusercontent.com/borismus/webvr-polyfill/master/build/webvr-polyfill.js
+echo "I'm not going to be able to do that, Dave. You don't want to corrupt your working VR do you?"
+
+#curl -o js/lib/VRControls.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/VRControls.js
+#curl -o js/lib/VREffect.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/effects/VREffect.js
+#curl -o js/lib/three.min.js http://threejs.org/build/three.min.js
+#curl -o js/lib/webvr-polyfill.js https://raw.githubusercontent.com/borismus/webvr-polyfill/master/build/webvr-polyfill.js
